34dcadefaf7a647f75f889588a8b037d1d82d08f
Cloud code <110151168+Cloud-stack-pro@users.noreply.github.com>
2023-05-22 16:25:12 +0700

    inital commit

---
+++ useState.js
@@ -0,0 +1,26 @@
+/**
+ * @param { * } initalValue
+ * @returns { [ value:any, setValue:function, subscribe:function, unsubscribe:function ] }
+ */
+const useState = function useState( initalValue = !1 ) {
+
+    let rootValue = initalValue, listeners = [];
+
+    function setValue( value ) {
+        rootValue = value;
+        listeners.forEach( listener => listener( rootValue ) );
+        return !0
+    }
+    function subscribe( listener ) {
+        listeners.push( listener );
+        return !0
+    }
+    function unsubscribe( listener ) {
+        listeners = listeners.filter( i => i !== listener );
+        return !0
+    }
+
+    return [ rootValue, setValue, subscribe, unsubscribe ]
+}
+
+const [ value, setValue ] = useState;
